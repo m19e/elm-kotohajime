479ac3d7e9e65e3faa99e5bcdad799dff3673aa5
m19e <nexus.me7290@gmail.com>
2020-11-02 19:24:33 +0900

    Follow official guide sample

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,25 @@
-module Main exposing (main)
+module Main exposing (..)
 
-import Html exposing (..)
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
 
 main =
-    div [] [text "Hello world"]
+  Browser.sandbox { init = 0, update = update, view = view }
+
+type Msg = Increment | Decrement
+
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (String.fromInt model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
